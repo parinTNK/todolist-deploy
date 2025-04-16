e1a4b46b220ba4d0106bcc57b86cb6c62e7c0213
parinTNK <parin.tnk@gmail.com>
2025-04-17 00:13:57 +0700

    feat: todo list home page

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -1,18 +1,24 @@
-import experss from "express";
+import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
 import connectDB from "./db.mjs";
 import todoRoute from "./routes/todoRoute.mjs";
 
-const app = experss();
+const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(cors());
-app.use(experss.json());
+app.use(express.json());
 app.use(morgan("dev"));
 
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Todo API");
+});
+
+//localhost:5000/api/todos
 app.use("/api/todos", todoRoute);
 
 app.listen(PORT, () => {
