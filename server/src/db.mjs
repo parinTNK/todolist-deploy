696ba37ab9993d08eab14426642165ff3d956ccf
parinTNK <parin.tnk@gmail.com>
2025-04-17 20:39:13 +0700

    test deploy node to server

--- server/src/db.mjs
+++ server/src/db.mjs
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB', err);
