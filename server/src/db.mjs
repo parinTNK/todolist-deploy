6720eeea2006472f80625b31d62ad7e98349e06e
parinTNK <parin.tnk@gmail.com>
2025-04-20 11:01:41 +0700

    2
    3
    
    4

--- server/src/db.mjs
+++ server/src/db.mjs
@@ -6,9 +6,9 @@ dotenv.config();
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URL);
-        console.log('Connecหกดtedd to DB');
+        console.log('Connecหกดtedd to DBffff');
     } catch (err) {
-        console.error('Error dconnecting to DB', err);
+        console.error('Error dconndfectdfdfdfing to DB', err);
         process.exit(1);
     }
 };
